dc12e0e4482a30443f012af1de21363d0aa27a65
Akpomena <akpomenaojefia@gmail.com>
2023-12-05 10:56:04 +0100

    Added face culling

--- 3D Engine/src/Engine/Renderer/Renderer.cpp
+++ 3D Engine/src/Engine/Renderer/Renderer.cpp
@@ -19,6 +19,10 @@ void Engine::Renderer::Init()
 
 	glEnable(GL_BLEND);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
+	glFrontFace(GL_CCW);
 }
 
 void Engine::Renderer::BeginScene(Camera& camera, Shader& shader)
